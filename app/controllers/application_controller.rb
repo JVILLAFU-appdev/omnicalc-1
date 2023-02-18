3014c1a6817cd7896b7833a074ac18e15bf6e2f3
JVILLAFU <JVILLAFU@CHICAGOBOOTH.EDU>
2023-02-18 01:52:37 +0000

    RCAV

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
 
+  def homepage
+    render({:template =>"calculation_templates/homepage.html.erb"})
+  end
+
+  def square_form
+    render({:template =>"calculation_templates/square_form.html.erb"})
+  end
+
+  def square_root_form
+    render({:template =>"calculation_templates/square_root_form.html.erb"})
+  end
+
+  def payment_form
+    render({:template =>"calculation_templates/payment_form.html.erb"})
+  end
+
+  def random_form
+    render({:template =>"calculation_templates/random_form.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
 
+  get("/", {:controller => "application", :action => "homepage"})
+
+  get("/square/new", {:controller => "application", :action => "square_form"})
+
+  get("/square_root/new", {:controller => "application", :action => "square_root_form"})
+
+  get("/payment/new", {:controller => "application", :action => "payment_form"})
+
+  get("/random/new", {:controller => "application", :action => "random_form"})
+
 end
